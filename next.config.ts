cfa2afc314eb83dbc918419317d6cc8a6a1edde7
Jeet <jetdeula@gmail.com>
2025-06-09 17:48:00 +0545

    config external link images

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,26 @@ const nextConfig: NextConfig = {
         hostname: "images.unsplash.com",
         port: "",
       },
+      {
+        protocol: "https",
+        hostname: "fakestoreapi.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "letsenhance.io",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "blog.daraz.com.np",
+        port: "",
+      },
     ],
   },
 };
