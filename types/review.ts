75e67c0d04d8568e74079edfd37d3eb11e02e4da
Jeet <jetdeula@gmail.com>
2025-06-17 11:40:24 +0545

    init : new types

--- types/review.ts
+++ types/review.ts
@@ -1,4 +1,17 @@
-export type reviewtype = {
-  id: number;
+export type User = {
+  id: string;
+  name: string;
+  email: string;
+  avatar: string | null;
+};
+
+export type reviewType = {
+  id: string;
+  rating: number;
   comment: string;
+  images: string[];
+  date: string;
+  productId: string;
+  userId: string;
+  user: User;
 };
