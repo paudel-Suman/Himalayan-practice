b9c114c1bdb2baeef7a9eb5b72dd622771557725
Jeet <jetdeula@gmail.com>
2025-06-22 10:27:56 +0545

    add : new katunje s3 image config

--- next.config.ts
+++ next.config.ts
@@ -43,6 +43,11 @@ const nextConfig: NextConfig = {
         hostname: "img.freepik.com",
         port: "",
       },
+      {
+        protocol: "https",
+        hostname: "katunje.s3.ap-south-1.amazonaws.com",
+        port: "",
+      },
     ],
   },
 };
