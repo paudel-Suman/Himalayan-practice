dc61a0194c298ec827de77d1468b9db47c95c3f1
Jeet <jetdeula@gmail.com>
2025-06-30 16:59:39 +0545

    justify -center category

--- app/(root)/home/category.tsx
+++ app/(root)/home/category.tsx
@@ -9,21 +9,21 @@ const Category = ({ category }: { category: categoryType[] }) => {
     <div>
       <PageHeader title="Explore Our Category" />
 
-      <div className="grid lg:grid-cols-5 md:grid-cols-3 grid-cols-2 gap-6 my-10">
+      <div className="sm:flex grid grid-cols-2 flex-wrap gap-6 my-8 justify-center">
         {category.slice(0, 8).map((item, index) => (
           <Link
             href={`/search?categoryId=${item.id}`}
             key={index}
             className="relative group cursor-pointer"
           >
-            <div className="absolute inset-0 bg-gradient-to-t from-black/50 via-black/20 to-transparent rounded-md" />
+            <div className="sm:w-[15em] absolute inset-0 bg-gradient-to-t from-black/50 via-black/20 to-transparent rounded-md" />
             <figure className="rounded-md">
               <Image
                 src={item?.image}
                 alt={item.name}
                 width={1000}
                 height={1000}
-                className="h-[10em] object-cover rounded-md group-hover:border-2 group-hover:border-red-600 group-hover:sclae-125 ease-in-out duration-200"
+                className="sm:h-[10em] h-[8em] sm:w-[15em] object-cover rounded-md group-hover:border-2 group-hover:border-red-600 group-hover:sclae-125 ease-in-out duration-200"
               />
             </figure>
             <div className="absolute left-4 bottom-4 text-white">
