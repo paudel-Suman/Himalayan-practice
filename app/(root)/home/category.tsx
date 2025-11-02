3c3f742a18096cbf544366b759a418a160161d58
paudel-Suman <sumanpaudel227@gmail.com>
2025-11-02 11:16:38 +0545

    fd

--- app/(root)/home/category.tsx
+++ app/(root)/home/category.tsx
@@ -21,7 +21,7 @@ const Category = ({ category }: { category: categoryType[] }) => {
               <Image
                 src={item?.image}
                 alt={item.name}
-                width={2000}
+                width={1000}
                 height={1000}
                 className="sm:h-[10em] h-[8em] sm:w-[12em] object-contain  rounded-md   group-hover:scale-125 ease-in-out duration-200"
               />
